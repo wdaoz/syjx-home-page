6b27e876b78ec0529dfe5c7fc6e9f0b3aff8b64b
WDAOZHASEE <2847317085@qq.com>
2025-04-30 11:11:35 +0800

    xiugai

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
   //网页元数据
   metaData:{
-    title: 'Leleo的个人主页🎉',
-    description: '欢迎来到Leleo的奇妙世界！',
+    title: '水月镜心的个人主页🎉',
+    description: '星星之火，可以燎原',
     keywords: 'Leleo,leleo,个人主页,个人网站',
     icon: '/favicon.ico'   //网页图标，支持外链
   },
 
   avatar: "/img/avatar.jpg", // 头像
-  welcometitle: "Hi, I'm Leleo", // 标题
+  welcometitle: "Hi, I'm SYJX", // 标题
 
   // 颜色配置
   color: {
